4bb38c8a45022c76d98c7e18bcb7f14b2371dd8d
jahan <jahanzaibkhan239@gmail.com>
2022-05-16 15:45:13 +0500

    Tasks List with Show/Hide Description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,7 @@
         </div>
 
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/34.0.0/classic/ckeditor.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.5/perfect-scrollbar.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" crossorigin="anonymous"></script>
--- resources/views/users/checklists/show.blade.php
+++ resources/views/users/checklists/show.blade.php
@@ -7,8 +7,30 @@
             <div class="col-md-12">
                 <div class="card">
 
-                    <div class="card-body">
+                    <div class="card-header">
                         {{ $checklist->name }}
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            @foreach($checklist->tasks as $task)
+                                <tr>
+                                    <td></td>
+                                    <td class="task-description-toggle" data-id="{{ $task->id }}">{{ $task->name }}</td>
+                                    <td>
+                                        <svg class="icon" id="task-caret-bottom-{{ $task->id }}">
+                                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-caret-bottom') }}"></use>
+                                        </svg>
+                                        <svg class="icon d-none" id="task-caret-top-{{ $task->id }}">
+                                            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-caret-top') }}"></use>
+                                        </svg>
+                                    </td>
+                                </tr>
+                                <tr class="d-none" id="task-description-{{ $task->id }}">
+                                    <td></td>
+                                    <td colspan="2" >{!! $task->description !!}</td>
+                                </tr>
+                            @endforeach
+                        </table>
                     </div>
 
                 </div>
@@ -16,6 +38,18 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(function () {
+            $('.task-description-toggle').click(function() {
+                $('#task-description-' + $(this).data('id')).toggleClass('d-none');
+                $('#task-caret-bottom-' + $(this).data('id')).toggleClass('d-none');
+                $('#task-caret-top-' + $(this).data('id')).toggleClass('d-none');
+            });
+        });
+    </script>
 @endsection
 
 
